2d8fcee7df2bbe762c6b4a503abd308952566c76
Felicity Brammer <felicitybrammer@gmail.com>
2021-12-22 17:00:51 -0700

    user model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, Types, model } = require('mongoose');
+
+const UserSchema = new Schema({
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: () => Promise.resolve(false),
+                message: 'Not a valid email'
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);  
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friend
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
