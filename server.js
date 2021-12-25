b4965ad4acb0af3d4296e8081db5ea33446c0cb7
Felicity Brammer <felicitybrammer@gmail.com>
2021-12-25 15:58:50 -0700

    user-routes

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use(express.static('public'));
+
+app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/my-never-private-thoughts', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
