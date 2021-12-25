27f14ee9992ec7685b1df5e2f12fc5cb8376cb18
Felicity Brammer <felicitybrammer@gmail.com>
2021-12-25 16:39:47 -0700

    thought, reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,68 @@
+const { Schema, model, Types } = require('mongoose');
+//date format function??
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now, /////
+        get: createdAtVal => dateFormatFunction(createdAtVal) //////
+    }
+},
+{
+    toJSON: {
+        getters: true
+    }
+});
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,//
+        get: (createdAtVal) => dateFormatFunction(createdAtVal)////
+        // Date
+// Set default value to the current timestamp
+// Use a getter method to format the timestamp on query    
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
+
+
+
